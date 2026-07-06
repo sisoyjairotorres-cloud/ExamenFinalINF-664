808ff34d1ec9d6cd67eadc18fbcaf3546bde5e30
sisoyjairotorres-cloud <sisoyjairotorres@gmail.com>
2026-07-06 21:07:20 +0000

    primer commit

---
+++ info_equipo.py
@@ -0,0 +1,14 @@
+nombre = input("Introduce tu nombre: ")
+pais = input("Introduce tu pais: ")
+descripcion = input("Explicate en una breve descripcion: ")
+
+def presentar(mi_nombre,mi_pais,mi_descripcion):
+            print("El usuario " 
+                + str(mi_nombre) + " vieve en " + str(mi_pais) + " se describio a si mismo como: "
+                + str(mi_descripcion))
+            return
+presentar(nombre,pais,descripcion)
+
+#ejemplos
+presentar("Juan","Bolivia ","Amable con ganas de trabajar")
+presentar("Alicia","Francia","En un futuro sere una gran pintora")
